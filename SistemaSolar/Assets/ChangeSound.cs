7eb112fd3206fba1a2a7d82196e4a04614a07e83
gabibatista <gabriela.nerybatista@gmail.com>
2017-10-20 10:39:23 -0200

    adding 'SolarSystem' and 'RollerMadness' games

---
+++ SistemaSolar/Assets/ChangeSound.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSound : MonoBehaviour {
+
+    static public GameObject target;
+    static GameObject oldTarget;
+
+	// Use this for initialization
+	void Start () {
+	    if (target == null)
+        {
+            target = GameObject.Find("Sun");
+            oldTarget = target;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (oldTarget != null)
+        {
+            if (target.name != oldTarget.name)
+            {
+                oldTarget.GetComponent<AudioSource>().Stop();
+                oldTarget = target;
+
+                target.GetComponent<AudioSource>().Play();
+            }
+        }
+	}
+}
